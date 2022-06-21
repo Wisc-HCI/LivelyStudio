81dbae35daeadfa17e19f7c0cb57d3853f33df50
Andrew Schoen <schoen.andrewj@gmail.com>
2022-06-21 17:46:55 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Grid, Box } from "grommet";
+import { getTheme } from "./theme";
+// import { Scene } from 'robot-scene';
 
 function App() {
+  const primaryColor = "#c5050c";
+  const theme = getTheme(primaryColor);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet full theme={theme}>
+      <Grid
+        fill
+        rows={["xxsmall", "flex"]}
+        columns={["flex", "flex"]}
+        gap="xsmall"
+        areas={[
+          { name: "header", start: [0, 0], end: [1, 0] },
+          { name: "nav", start: [0, 1], end: [0, 1] },
+          { name: "main", start: [1, 1], end: [1, 1] },
+        ]}
+      >
+        <Box gridArea="header" background="#9b0000" justify='center' pad='small'>Puppeteer</Box>
+        <Box gridArea="nav" round='xsmall' background="#44444477">
+        {/* <Scene
+              displayTfs={true}
+              displayGrid={true}
+              isPolar={false}
+              backgroundColor='#1e1e1e'
+              planeColor='#141414'
+              highlightColor={primaryColor}
+              plane={-0.75}
+              fov={50}
+              // store={useStore}
+              // onPointerMissed={clearFocus}
+              // paused={paused}
+          /> */}
+        </Box>
+        <Box gridArea="main" round='xsmall' background="#44444477"/>
+      </Grid>
+    </Grommet>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,86 @@
+export const getTheme = (primaryColor) => ({
+    name: "CoFrame",
+    rounding: 4,
+    defaultMode: "dark",
+    global: {
+      colors: {
+        brand: primaryColor,
+        background: "#000000",
+        control: primaryColor,
+      },
+      font: {
+        family: "Helvetica",
+      },
+      focus: {
+        border: {
+          color: primaryColor,
+        },
+      },
+      input: {
+        padding: 4,
+        extend: { backgroundColor: "#FFFFFF55" },
+      },
+      // edgeSize: {large: 50, small: 10, medium: 15}
+    },
+    button: {
+      border: {
+        radius: "4px",
+      },
+    },
+    radioButton: {
+      size: "16px",
+      border: { color: "#00000088" },
+    },
+    checkBox: {
+      size: "20px",
+      border: { color: "#00000088" },
+      color: primaryColor,
+      hover: { border: { color: "#00000088" } },
+    },
+    textInput: {
+      disabled: { opacity: 1 },
+    },
+    notification: {
+      toast: {
+        container: {
+          elevation: "none",
+        },
+      },
+      container: {
+        border: { color: "lightgrey" },
+        background: {
+          color: "background-front",
+        },
+      },
+    },
+    tab: {
+      active: {
+        background: primaryColor,
+        color: "dark-1",
+      },
+      background: "dark-3",
+      border: undefined,
+      color: "white",
+      hover: {
+        background: "#444444",
+        color: "white",
+      },
+      margin: undefined,
+      pad: {
+        bottom: undefined,
+        horizontal: "small",
+      },
+      extend: {
+        borderRadius: 4,
+        padding: 6,
+      },
+    },
+    tabs: {
+      gap: "medium",
+      header: {
+        extend: { padding: 10 },
+      },
+      panel: { padding: 10 },
+      extend: { padding: 10 },
+    },
+  })
