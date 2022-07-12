7f378ed5c2be1d2b94a8e32dbc8e7d5d4e8a1ed0
dsullivan117 <80975185+dsullivan117@users.noreply.github.com>
2022-07-11 20:30:51 -0500

    Add files via upload
    Modified programSpec to include new node types (i.e. state, goal, power-on, power-off).

--- src/programSpec.js
+++ src/programSpec.js
@@ -1,3 +1,38 @@
+//Tasks:
+    //Make goals (DONE):
+    //  Boots and hats will likely be like goals (which we think will be instances??)- switch this to goals
+    //
+    //Make states (DONE):
+    //A state is a collection of objectives which are filled with goals
+    //
+    //Make power on and off nodes (TODO):
+    //State nodes, goal nodes, power on node, power off node (goals will have multiple iterations)
+
+    //Questions:
+    //Where do functions come into play within the framework we are developing?
+
+    //Multiple groups of goals:
+    //  Cartesian Control Goals
+    //    Position (X, Y, Z)
+    //  Joint Control Goals
+    //    Rotate (yaw, pitch, roll)
+    //  General Goals
+    //    Collision avooidance, smoothness (jerk, velocity, accelartion minimzation) 
+    //  Liveliness Goals
+    //    Sway
+
+    //state has all of the joint values at a given that time that satisfy the goal (with certain weights)
+    //goal would be the cartesian values
+
+    //When you create a solver, you have to pass in all the objectives
+
+    //each objective has a type of goal it accepts
+
+    //The length of objectives has to equal the length of goals and weights
+
+    //The goals that user provide are a hybrid of the goals and objectives
+
+
 import { DATA_TYPES, CONNECTIONS, EXTRA_TYPES, TYPES, SIMPLE_PROPERTY_TYPES } from "simple-vp";
 import {
     FiClipboard,
@@ -14,27 +49,30 @@ export const programSpec = {
     {
       title: "Structures",
       dataType: DATA_TYPES.INSTANCE,
-      objectTypes: ["functionType", "operationType"],
+      objectTypes: ["stateType", "powerOnType", "powerOffType"],
       icon: FiClipboard,
     },
+    //Moved "States" to structures, but kept old function code for reference
+    //{
+    //  title: "Functions",
+    //  dataType: DATA_TYPES.CALL,
+    //  objectType: "functionType",
+    //  icon: FiLogOut,
+    //},
     {
-      title: "Functions",
-      dataType: DATA_TYPES.CALL,
-      objectType: "functionType",
-      icon: FiLogOut,
-    },
-    {
-      title: "Hats",
+      //Changed "Hat" to "Goals" and added two goal types.
+      title: "Goals",
       dataType: DATA_TYPES.INSTANCE,
-      objectTypes: ["hatType"],
-      icon: FiGrid,
+      objectTypes: ["goalTypeRot", "goalTypePos"],
+      icon: FiGrid, //
     },
-    {
-      title: "Boots",
-      dataType: DATA_TYPES.INSTANCE,
-      objectTypes: ["bootType"],
-      icon: FiBox,
-    },
+    //Commented out Boots as this will no longer be needed.
+    //{
+    //  title: "Boots", //Can get rid of boots
+    //  dataType: DATA_TYPES.INSTANCE,
+    //  objectTypes: ["bootType"],
+    //  icon: FiBox,
+    //},
   ],
   objectTypes: {
     programType: {
@@ -296,5 +334,191 @@ export const programSpec = {
         ],
       },
     },
+    //New goal, rotate: this can be one of several goal type.------------------------------------------
+    goalTypeRot: {
+      name: "Goal: Rotate",
+      type: TYPES.OBJECT,
+      referenceBlock: {
+        onCanvas: false,
+        color: "#bdb655",
+        icon: FiGrid,
+        extras: [
+          EXTRA_TYPES.LOCKED_INDICATOR,
+          {
+            icon: FiMoreHorizontal,
+            type: EXTRA_TYPES.DROPDOWN,
+            contents: [
+              EXTRA_TYPES.DELETE_BUTTON,
+              EXTRA_TYPES.DEBUG_TOGGLE,
+              EXTRA_TYPES.NAME_EDIT_TOGGLE,
+              EXTRA_TYPES.SELECTION_TOGGLE,
+            ],
+          },
+        ],
+      },
+      instanceBlock: {
+        onCanvas: false,
+        color: "#bdb655",
+        icon: FiGrid,
+        extras: [
+          EXTRA_TYPES.LOCKED_INDICATOR,
+          {
+            icon: FiMoreHorizontal,
+            type: EXTRA_TYPES.DROPDOWN,
+            contents: [
+              EXTRA_TYPES.DELETE_BUTTON,
+              EXTRA_TYPES.DEBUG_TOGGLE,
+              EXTRA_TYPES.NAME_EDIT_TOGGLE,
+              EXTRA_TYPES.SELECTION_TOGGLE,
+            ],
+          },
+        ],
+      },
+    },
+    //New goal, position.------------------------------------------------------------------------------
+    goalTypePos: {
+      name: "Goal: Position",
+      type: TYPES.OBJECT,
+      referenceBlock: {
+        onCanvas: false,
+        color: "#bdb655",
+        icon: FiGrid,
+        extras: [
+          EXTRA_TYPES.LOCKED_INDICATOR,
+          {
+            icon: FiMoreHorizontal,
+            type: EXTRA_TYPES.DROPDOWN,
+            contents: [
+              EXTRA_TYPES.DELETE_BUTTON,
+              EXTRA_TYPES.DEBUG_TOGGLE,
+              EXTRA_TYPES.NAME_EDIT_TOGGLE,
+              EXTRA_TYPES.SELECTION_TOGGLE,
+            ],
+          },
+        ],
+      },
+      instanceBlock: {
+        onCanvas: false,
+        color: "#bdb655",
+        icon: FiGrid,
+        extras: [
+          EXTRA_TYPES.LOCKED_INDICATOR,
+          {
+            icon: FiMoreHorizontal,
+            type: EXTRA_TYPES.DROPDOWN,
+            contents: [
+              EXTRA_TYPES.DELETE_BUTTON,
+              EXTRA_TYPES.DEBUG_TOGGLE,
+              EXTRA_TYPES.NAME_EDIT_TOGGLE,
+              EXTRA_TYPES.SELECTION_TOGGLE,
+            ],
+          },
+        ],
+      },
+    },
+    //State type----------------------------------------------------------------------------------------
+    stateType: {
+      name: "State",
+      type: TYPES.OBJECT,
+      instanceBlock: {
+        onCanvas: true,
+        color: "#7dd6ff",
+        icon: FiClipboard,
+        connections: {
+          bottom: {
+            direction: CONNECTIONS.OUTBOUND,
+            allowed: ["stateType", "powerOffType"],
+            limitOne: false,
+          },
+          top: {
+            direction: CONNECTIONS.INBOUND,
+            allowed: ["stateType", "operationType", "programType", "powerOnType"],
+            limitOne: false,
+          },
+        },
+        extras: [
+          EXTRA_TYPES.LOCKED_INDICATOR,
+          {
+            icon: FiMoreHorizontal,
+            type: EXTRA_TYPES.DROPDOWN,
+            contents: [
+              EXTRA_TYPES.DELETE_BUTTON,
+              EXTRA_TYPES.DEBUG_TOGGLE,
+              EXTRA_TYPES.SELECTION_TOGGLE,
+            ],
+          },
+        ],
+        hideNewPrefix: true,
+      },
+      properties: {
+        children: {
+          name: "Children",
+          accepts: ["goalTypePos", "goalTypeRot"],
+          default: [],
+          isList: true,
+          fullWidth: true,
+        },
+      },
+    },
+    //Power On Node--------------------------------------------------------------------------------------
+    powerOnType: {
+      name: "PowerOn",
+      type: TYPES.OBJECT,
+      instanceBlock: {
+        onCanvas: true,
+        color: "#eb4444",
+        icon: FiClipboard,
+        connections: {
+          bottom: {
+            direction: CONNECTIONS.OUTBOUND,
+            allowed: ["stateType"],
+            limitOne: false,
+          },
+        },
+        extras: [
+          EXTRA_TYPES.LOCKED_INDICATOR,
+          {
+            icon: FiMoreHorizontal,
+            type: EXTRA_TYPES.DROPDOWN,
+            contents: [
+              EXTRA_TYPES.DELETE_BUTTON,
+              EXTRA_TYPES.DEBUG_TOGGLE,
+              EXTRA_TYPES.SELECTION_TOGGLE,
+            ],
+          },
+        ],
+        hideNewPrefix: true,
+      },
+    },
+    //Power Off Node-------------------------------------------------------------------------------------
+    powerOffType: {
+      name: "Power Off",
+      type: TYPES.OBJECT,
+      instanceBlock: {
+        onCanvas: true,
+        color: "#7d746f",
+        icon: FiClipboard,
+        connections: {
+          top: {
+            direction: CONNECTIONS.INBOUND,
+            allowed: ["stateType", "operationType", "programType"],
+            limitOne: false,
+          },
+        },
+        extras: [
+          EXTRA_TYPES.LOCKED_INDICATOR,
+          {
+            icon: FiMoreHorizontal,
+            type: EXTRA_TYPES.DROPDOWN,
+            contents: [
+              EXTRA_TYPES.DELETE_BUTTON,
+              EXTRA_TYPES.DEBUG_TOGGLE,
+              EXTRA_TYPES.SELECTION_TOGGLE,
+            ],
+          },
+        ],
+        hideNewPrefix: true,
+      },
+    }
   },
 };
